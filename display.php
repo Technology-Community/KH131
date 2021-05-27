cb98cf7b4573bb9aa1a6efeb5c5540106d6d2762
KingNNT <Dev.KingNNT@gmail.com>
2021-05-27 16:21:29 +0700

    Update

--- display.php
+++ display.php
@@ -56,12 +56,18 @@ $time = subDayswithdate($value, 10);
                     <?php
                     $row = 1;
                     if (($handle = fopen("who_covid_southeast.csv", "r")) !== FALSE) {
-                    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                    $num = count($data);
-                    echo "<p> $num fields in line $row: <br /></p>\n";
-                    $row++;
-                    for ($c=0; $c < $num; $c++) { echo $data[$c] . "<br />\n" ; } } fclose($handle); } </table>
+                        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                            $num = count($data);
+                            echo "<p> $num fields in line $row: <br /></p>\n";
+                            $row++;
+                            for ($c = 0; $c < $num; $c++) {
+                                echo $data[$c] . "<br />\n";
+                            }
+                        }
+                        fclose($handle);
+                    }
                     ?>
+                </table>
             </div>
         </div>
     </main>
